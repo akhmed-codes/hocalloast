28547edd0a61880d018b02d7220d63ae0f3a68e0
akhmed-codes <cubicsstack@gmail.com>
2021-10-11 15:40:00 +0530

    added html formatting

--- api/main.js
+++ api/main.js
@@ -3,14 +3,31 @@ const { join } = require('path')
 
 module.exports = (req, res) => {
 
+    let html_head = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>🤖 LocalHoast</title>
+        <link rel="stylesheet" href="style.css">
+      </head>
+      <body>`;
+    
+    let html_body = `
+        <script src="index.js"></script>
+        </body>
+    </html>`
+
     const { name = 'demo.html' } = req.query;
 
-    fs.readFile(join(__dirname, '_files', name), 'utf8', (err, data) => {
+    fs.readFile(join(__dirname, '_files', `${name}.html`), 'utf8', (err, data) => {
         if (err) {
             console.error(err)
             return
         }
-        res.status(200).send(`${data}!`);
+        res.status(200).send(`${html_head} ${data} ${html_body}`);
     });
 
 }
