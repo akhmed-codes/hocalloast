7f802346c1067d2d047edaeb46d6c1b08c1e0414
akhmed-codes <cubicsstack@gmail.com>
2021-10-11 15:25:48 +0530

    initial commit

---
+++ api/main.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const { join } = require('path')
+
+module.exports = (req, res) => {
+
+    const { name = 'demo' } = req.query;
+
+    fs.readFile(join(__dirname, '_files', name), 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        res.status(200).send(`${data}!`);
+    });
+
+}
